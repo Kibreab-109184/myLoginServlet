cc9c7a7671c9c841f9d94b37252617fc20b75fda
kibreab-109184 <kiansemomo@gmail.com]>
2018-09-21 19:22:20 -0500

    Initial commit

---
+++ src/main/java/UserDb.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class UserDb {
+    HashMap<String,User> users;
+
+    public UserDb() {
+
+        users= new HashMap<>();
+
+        users.put("john",new User("john","123"));
+        users.put("whiteny",new User("whiteny","123"));
+        users.put("abebe",new User("abebe","123"));
+    }
+
+
+
+    public HashMap<String,User> getUsers(){
+
+     return users;
+
+    }
+
+
+}
